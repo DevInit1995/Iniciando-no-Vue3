3623b175cd0caaa08567b64e1c8638cfcf1a554b
Jhon Santos <kennedyalves871@gmail.com>
2023-11-14 19:55:52 -0300

    Entendendo os Lifecycle hooks

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -2,21 +2,24 @@
     <div>
         <PrimeiroComponente/>
         <p>O dado do app é: {{ testando }}</p>
+        <LifeCycle/>
     </div>
 </template>
 
 <script>
-import PrimeiroComponente from './components/PrimeiroComponente.vue'
+    import PrimeiroComponente from './components/PrimeiroComponente.vue'
+    import LifeCycle from './components/LifeCycle.vue';
 
-    export default {
-        name: 'App',
-        components: {
-            PrimeiroComponente
-        },
-        data(){
-            return {
-                testando: "Teste",
+        export default {
+            name: 'App',
+            components: {
+                PrimeiroComponente,
+                LifeCycle
+            },
+            data() {
+                return {
+                    testando: "Teste",
+                }
             }
         }
-    }
 </script>
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCicle',
+    data() {
+        return {
+            nome: 'Ainda não sei',
+        }
+    },
+    created() {
+       setTimeout(() => {
+        this.nome = 'Jhon Santos'
+       }, 1000);
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = 'Jhon Jhon'
+        }, 2000);
+    }
+}
+</script>
