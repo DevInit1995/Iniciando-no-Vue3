f4a1538e2a3718aa4bad9e0159254159d3a4a386
Jhon Santos <kennedyalves871@gmail.com>
2023-11-19 22:13:56 -0300

    Atributos dinâmicos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -2,7 +2,9 @@
 <template>
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else-if="text">Teste</p>
         <p v-else>Estou em busca de novas oportunidades!</p>
+       
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>JavaScript</li>
@@ -10,16 +12,25 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portifólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <Picture />
     </div>
 </template>
 <script>
+import Picture from './Picture.vue'
+
 export default {
     name: 'Info',
+    components: {
+        Picture
+    },
     data() {
         return {
             esta_trabalhando: false,
             mostrar_email: true,
-            email: 'jhon@exemple.com'
+            email: 'jhon@exemple.com',
+            text: false,
+            meu_link: 'https://google.com'
         }
     }
 }
---
+++ 2_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <!--sintaxe com v-bind resumido com :-->
+    <img :src="avatar" :alt="descricao">
+</template>
+
+<script>
+export default {
+    name: 'Picture',
+    data() {
+        return {
+            avatar: "/Img/avatar.png",
+            descricao: "Jhon Santos"
+        }
+    }
+}
+</script>
